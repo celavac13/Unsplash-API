c32e5aed6c327940b5a24c01cb29e50002eb11a3
stefan stevanovic <stefan713@live.com>
2022-04-01 17:30:43 +0200

    add click on img, and extract function for modal rendering

--- script.js
+++ script.js
@@ -32,6 +32,48 @@ modal.addEventListener("click", (e) => {
     }
 });
 
+// function for filling modal
+const renderingModal = function (data) {
+    modal.style.display = "block";
+    modal.style.opacity = "1";
+
+    modalImg.src = data.urls.regular;
+    modalImg.alt = data.alt_description ? data.alt_description : "Unsplash photo";
+
+    imgLink.href = imgLink.textContent = data.links.html;
+    likes.textContent = data.likes;
+    portrait.src = data.user.profile_image.medium;
+    userName.textContent = data.user.username;
+    portfolio.href = portfolio.textContent = data.user.portfolio_url;
+
+    // checking for social and rendering
+    socails.innerHTML = "";
+    if (data.user.instagram_username) {
+        const linkSocial = document.createElement("a");
+        linkSocial.href = `https://www.instagram.com/${data.user.instagram_username}`;
+        linkSocial.target = "_blank";
+        linkSocial.textContent = `Instagram`;
+        linkSocial.classList.add("modal-link");
+        socails.appendChild(linkSocial);
+    }
+    if (data.user.paypal_email) {
+        const linkSocial = document.createElement("a");
+        linkSocial.href = data.user.paypal_email;
+        linkSocial.target = "_blank";
+        linkSocial.textContent = `PayPal`;
+        linkSocial.classList.add("modal-link");
+        socails.appendChild(linkSocial);
+    }
+    if (data.user.twitter_username) {
+        const linkSocial = document.createElement("a");
+        linkSocial.href = `https://www.twitter.com/${el.user.twitter_username}`;
+        linkSocial.target = "_blank";
+        linkSocial.textContent = `Twitter`;
+        linkSocial.classList.add("modal-link");
+        socails.appendChild(linkSocial);
+    }
+}
+
 // function for geting photos from site and rendering them
 const getPhotos = function (page) {
     const request = new XMLHttpRequest;
@@ -45,9 +87,13 @@ const getPhotos = function (page) {
 
         // rendering images on page
         data.forEach(el => {
+            console.log(el);
             const img = `
                 <div class="card col-xl-4 col-lg-6 col-md-12 viewHandle">
-                    <img src="${el.urls.regular}" class="card-img-top" alt="${el.alt_description ? el.alt_description : "Unsplash photo"}">
+                    <a href="${el.urls.full}" target="_blank">
+                        <img src="${el.urls.regular}" class="card-img-top" alt="${el.alt_description ? el.alt_description : "Unsplash photo"}">
+                    </a>
+                    
                     <div class="card-body">
                         <div class="d-flex align-items-center">
                             <img src="${el.user.profile_image.medium}" class="card-img">
@@ -64,49 +110,15 @@ const getPhotos = function (page) {
                 </div>`
 
             holderImages.insertAdjacentHTML("beforeend", img);
+
+            // variables for selecting last added element
             const modalBtn = document.querySelectorAll(".modalBtn")[document.querySelectorAll(".modalBtn").length - 1];
+            const modalShowImg = document.querySelectorAll(".card-img-top")[document.querySelectorAll(".card-img-top").length - 1];
 
             // showing modal
             modalBtn.addEventListener("click", (e) => {
                 e.preventDefault();
-                modal.style.display = "block";
-                modal.style.opacity = "1";
-
-                modalImg.src = el.urls.regular;
-                modalImg.alt = el.alt_description ? el.alt_description : "Unsplash photo";
-
-                imgLink.href = imgLink.textContent = el.links.html;
-                likes.textContent = el.likes;
-                portrait.src = el.user.profile_image.medium;
-                userName.textContent = el.user.username;
-                portfolio.href = portfolio.textContent = el.user.portfolio_url;
-
-                // checking for social and rendering
-                socails.innerHTML = "";
-                if (el.user.instagram_username) {
-                    const linkSocial = document.createElement("a");
-                    linkSocial.href = `https://www.instagram.com/${el.user.instagram_username}`;
-                    linkSocial.target = "_blank";
-                    linkSocial.textContent = `Instagram`;
-                    linkSocial.classList.add("modal-link");
-                    socails.appendChild(linkSocial);
-                }
-                if (el.user.paypal_email) {
-                    const linkSocial = document.createElement("a");
-                    linkSocial.href = el.user.paypal_email;
-                    linkSocial.target = "_blank";
-                    linkSocial.textContent = `PayPal`;
-                    linkSocial.classList.add("modal-link");
-                    socails.appendChild(linkSocial);
-                }
-                if (el.user.twitter_username) {
-                    const linkSocial = document.createElement("a");
-                    linkSocial.href = `https://www.twitter.com/${el.user.twitter_username}`;
-                    linkSocial.target = "_blank";
-                    linkSocial.textContent = `Twitter`;
-                    linkSocial.classList.add("modal-link");
-                    socails.appendChild(linkSocial);
-                }
+                renderingModal(el);
             });
         });
     });
